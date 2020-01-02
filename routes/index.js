04ed64af0094c81be9a5cf4f93f3998bfc153c57
emir balcı <emir.balci@demirorenteknoloji.com>
2020-01-02 10:49:27 +0300

    json web token eklendi.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+const User = require("../models/User");
+
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/", (req, res, next) => {
+  const { username, password } = req.body;
+  bcryptjs.hash(password, 10).then(hash => {
+    const user = new User({
+      username,
+      password: hash
+    });
+
+    const promise = user.save();
+
+    promise
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+});
+
+router.post("/authenticate", (req, res, next) => {
+  const { username, password } = req.body;
+  User.findOne(
+    {
+      username
+    },
+    (err, user) => {
+      if (err) throw err;
+
+      if (!user) {
+        res.json({
+          status: false,
+          message: "Authentication failed, user not found"
+        });
+      } else {
+        bcryptjs.compare(password, user.password).then(result => {
+          if (!result) {
+            res.json({
+              status: false,
+              message: "Authentication failed, wrong password."
+            });
+          } else {
+            const payload = {
+              username
+            };
+            const token = jwt.sign(payload, req.app.get("api_secret_key"), {
+              expiresIn: 720
+            });
+
+            res.json({
+              status: true,
+              token
+            });
+          }
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
